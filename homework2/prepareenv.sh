ade07efad57428fab8c7ff5ba334eda0bee6ce4f
Cloud User <hema@homework.hawidq4ne02edgnqxdc33qhnsb.parx.internal.cloudapp.net>
2022-05-30 19:38:59 +0000

    completed the workflow remaining readme

--- homework2/prepareenv.sh
+++ homework2/prepareenv.sh
@@ -5,5 +5,7 @@ mkdir ./notebooks ./pgvol ./pgadmin
 chmod 777 ./pgadmin
 
 ## Prepare airflow setup
-mkdir ./dags ./logs ./plugins ./af_pgvol ./db
+mkdir ./dags ./logs ./plugins ./af_pgvol ./db ./result
 echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
+chmod 777 ./result
+cp HW2_DAG.py ./dags/
